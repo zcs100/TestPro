739c8ae0495523d5d89cd7c496fbae1b175a6cfd
sunzhichao <sunzhichao@lianjia.com>
2016-11-29 18:18:33 +0800

    init commit

---
+++ src/main/java/com/jdk/UsedClassTest.java
@@ -0,0 +1,15 @@
+package com.jdk;
+
+import org.junit.Test;
+
+/**
+ * Created by szc on 16/11/29.
+ */
+public class UsedClassTest {
+
+    @Test
+    public void hashMapTest(){
+        
+    }
+
+}
