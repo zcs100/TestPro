596b579e56516cec65ef34e6aa3e44fe9e70b3a1
sunzhichao <sunzhichao@lianjia.com>
2017-02-27 18:40:09 +0800

    update rpc demo

--- src/main/java/com/rpc/demo/RpcConsumer.java
+++ src/main/java/com/rpc/demo/RpcConsumer.java
@@ -7,11 +7,11 @@ public class RpcConsumer {
 
     public static void main(String[] args) throws Exception {
         HelloService service = RpcFramework.refer(HelloService.class, "127.0.0.1", 1234);
-        for (int i = 0; i < 1; i ++) {
-            String hello = service.hello("World" + i);
-            System.out.println(hello);
-            Thread.sleep(1000);
-        }
+
+        String hello = service.hello("World");
+        System.out.println(hello);
+        Thread.sleep(1000);
+
     }
 
 }
